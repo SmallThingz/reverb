3704e2f3b7b4e9e38825c58607a905a4a39dd432
ItsMeSamey <sameychain5041@gmail.com>
2026-09-12 23:34:12 +1000

    style: simplify library icon

--- app/src/main/java/app/smallthingz/reverb/AppIcons.kt
+++ app/src/main/java/app/smallthingz/reverb/AppIcons.kt
@@ -2,7 +2,7 @@ package app.smallthingz.reverb
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
-import androidx.compose.material.icons.automirrored.rounded.ListAlt
+import androidx.compose.material.icons.rounded.Dehaze
 import androidx.compose.material.icons.automirrored.rounded.Undo
 import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material.icons.rounded.AudioFile
@@ -50,7 +50,7 @@ internal object AppIcons {
     val seekForward: ImageVector = Icons.Rounded.Forward10
     val save: ImageVector = Icons.Rounded.SaveAlt
     val exportRange: ImageVector = exportRangeIcon
-    val library: ImageVector = Icons.AutoMirrored.Rounded.ListAlt
+    val library: ImageVector = Icons.Rounded.Dehaze
     val audioFile: ImageVector = Icons.Rounded.AudioFile
     val capture: ImageVector = Icons.Rounded.GraphicEq
     val oneShot: ImageVector = Icons.Rounded.LooksOne
