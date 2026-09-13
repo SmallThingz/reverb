009eb46c800f9729f3096119c5858e75cd689cc7
ItsMeSamey <sameychain5041@gmail.com>
2026-09-13 20:21:04 +1000

    fix: restore range selector semantics

--- app/src/main/java/app/smallthingz/reverb/CaptureScreen.kt
+++ app/src/main/java/app/smallthingz/reverb/CaptureScreen.kt
@@ -41,6 +41,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
