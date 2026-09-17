9c48d19a2479915e41e9c0a51db8b09515d6196c
ItsMeSamey <sameychain5041@gmail.com>
2026-09-17 13:38:53 +1000

    perf: move incident startup recovery off main

--- app/src/main/java/app/smallthingz/reverb/ReverbApplication.kt
+++ app/src/main/java/app/smallthingz/reverb/ReverbApplication.kt
@@ -1,12 +1,19 @@
 package app.smallthingz.reverb
 
 import android.app.Application
+import android.os.Process
 import android.util.Log
 
 class ReverbApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        runCatching { RecordingIncidentStore.recoverPriorSessionIfNeeded(this) }
-            .onFailure { Log.e("ReverbApplication", "Unable to recover prior recording incident", it) }
+        Thread(
+            {
+                runCatching { Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND) }
+                runCatching { RecordingIncidentStore.recoverPriorSessionIfNeeded(this) }
+                    .onFailure { Log.e("ReverbApplication", "Unable to recover prior recording incident", it) }
+            },
+            "reverb-incident-recovery",
+        ).apply { isDaemon = true }.start()
     }
 }
